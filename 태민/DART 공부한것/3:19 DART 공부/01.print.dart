5782fa6c0220ff4a19ff462aadbf6e823e68989d
Eunjun1 <ibbuni8486@gmail.com>
2025-03-21 17:52:01 +0900

    d

--- 태민/DART 공부한것/3:19 DART 공부/01.print.dart
+++ 태민/DART 공부한것/3:19 DART 공부/01.print.dart
@@ -2,22 +2,21 @@
 // 첫줄은 무조건 main
 // dart는 앞에 void를 안써도 괜찮음
 
-main(){
-  // 단축키 
+main() {
+  // 단축키
   // - 한줄 복사 : option(alt) + shift + 화살표
   // - 한줄 삭제 : cmd + shift + k
   // - 줄 이동 : option(alt) + 화살표
 
   // print는 개발 할 때만 사용
-  print('Hellow world!\nGood Morning!\nHow are you?\nOh You look so bad');
+  print('Hellow world! Good Morning! How are you? Oh You look so bad');
   print('Hellow World2!');
-
 
   // ""나 ''의 구분이 없다.
   // '안녕하세요'
   print("'안녕하세요!'");
   print('내 친구는 나에게 "잘 될거야!"라고 말했습니다.');
-  
-  print(1+2);
+
+  print(1 + 2);
   print('1+2');
-} 
+}
